f46dfd3d4872a3f44e5f772a2f2dc37d933562e8
KaitanKale <kaitan.ireland98@gmail.com>
2018-10-20 22:19:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lauren98gray/votervisionapp/Districts4and5.java
@@ -0,0 +1,14 @@
+package com.example.lauren98gray.votervisionapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Districts4and5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.candidate_activity);
+    }
+
+}
